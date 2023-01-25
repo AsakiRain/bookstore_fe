fe39a3ccc888d6a1c14af32f91b39eed42c9fe8c
AsakiRain <1287059631@qq.com>
2023-01-25 23:38:40 +0800

    chore: 分离Menu成为组件

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,58 +1,31 @@
 <template>
-  <div id="menu">
-    <div id="menu-title">菜单</div>
+  <div
+    id="menu"
+    class="self-start w-50 bg-white flex flex-col rounded-lg sticky top-20 z-1 shrink-0 overflow-hidden"
+  >
+    <div
+      id="menu-title"
+      class="self-stretch font-bold text-base text-center p-4 border-b border-b-solid border-b-gray-300"
+    >
+      菜单
+    </div>
     <div class="line-divider"></div>
-    <div id="menu-content">
-      <router-link class="block-link nav-link" to="/"> 全部商品 </router-link>
-      <router-link class="block-link nav-link" to="/search">
+    <div id="menu-content" class="flex flex-col items-stretch text-base">
+      <router-link class="nav-link block-link" to="/"> 全部商品 </router-link>
+      <router-link class="nav-link block-link" to="/search">
         搜索商品
       </router-link>
-      <router-link class="block-link nav-link" to="/cart"> 购物车 </router-link>
-      <router-link class="block-link nav-link" to="/order">
+      <router-link class="nav-link block-link" to="/cart"> 购物车 </router-link>
+      <router-link class="nav-link block-link" to="/order">
         订单管理
       </router-link>
-      <router-link class="block-link nav-link" to="/user">
+      <router-link class="nav-link block-link" to="/user">
         个人中心
       </router-link>
-      <div class="line-divider"></div>
-      <router-link class="block-link nav-link bold" to="/manage">
+      <router-link class="nav-link block-link border-t border-t-solid border-t-gray-300" to="/manage">
         后台管理
       </router-link>
     </div>
   </div>
 </template>
-<style lang="css" scoped>
-#menu {
-  align-self: start;
-  width: 200px;
-  background-color: var(--backgroud-color);
-  display: flex;
-  flex-direction: column;
-  border-radius: 8px;
-  position: sticky;
-  top: 80px;
-  z-index: 1;
-  flex-shrink: 0;
-  overflow: hidden;
-}
-
-#menu-title {
-  align-self: stretch;
-  font-weight: bold;
-  font-size: 16px;
-  text-align: center;
-  padding: 16px;
-  border-bottom: 1px solid var(--border-color);
-}
-
-#menu-content {
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  font-size: 16px;
-}
-.block-link {
-  padding: 12px 16px;
-}
-
-</style>
+<style lang="css" scoped></style>
